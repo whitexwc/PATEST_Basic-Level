1724d4313e18be42a87a6682de5c4c93db664595
wucheng xiao <thinker55@163.com>
2017-01-23 23:44:52 +0800

    1001. 害死人不偿命的(3n+1)猜想

---
+++ BL_1001.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class BL_1001 {
+	public static void main(String []args){
+		int i=0,n;
+		Scanner input=new Scanner(System.in);
+		n=input.nextInt();
+			while(n!=1){
+				if(n%2==0){
+					n/=2;
+				}
+				else{
+					n=(3*n+1)/2;
+				}
+				i++;
+			}
+		System.out.print(i);
+	}
+
+}
