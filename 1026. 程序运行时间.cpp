640148efc58b8edf29d4aba088c8cd84ae20d9ff
wucheng xiao <thinker55@163.com>
2017-02-20 15:48:42 +0800

    Update 1026. 程序运行时间.cpp

--- 1026. 程序运行时间.cpp
+++ 1026. 程序运行时间.cpp
@@ -3,12 +3,9 @@
 using namespace std;
 int main()
 {
-    int n,h,m,s;
+    int n;
     float c1,c2;
     cin>>c1>>c2;
     n=int((c2-c1)/100+0.5);
-    h=n/3600;
-    m=(n-3600*h)/60;
-    s=n-3600*h-60*m;
-    cout<<setw(2)<<setfill('0')<<h<<':'<<setw(2)<<setfill('0')<<m<<':'<<setw(2)<<setfill('0')<<s<<endl;
+    cout<<setw(2)<<setfill('0')<<n/3600<<':'<<setw(2)<<setfill('0')<<n%3600/60<<':'<<setw(2)<<setfill('0')<<n%60<<endl;
 }
