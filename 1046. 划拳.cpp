73a707f5e745e768a178bc31835c662390161923
wucheng xiao <thinker55@163.com>
2017-02-20 15:46:44 +0800

    Update 1046. 划拳.cpp

--- 1046. 划拳.cpp
+++ 1046. 划拳.cpp
@@ -2,18 +2,15 @@
 using namespace std;
 int main()
 {
-    int n,a=0,b=0;
+    int n,a=0,b=0,a1,a2,b1,b2;
     cin>>n;
-    int *a1=new int [n];
-    int *a2=new int [n];
-    int *b1=new int [n];
-    int *b2=new int [n];
     for(int i=0;i<n;i++)
     {
-        cin>>a1[i]>>a2[i]>>b1[i]>>b2[i];
-        if(a2[i]==a1[i]+b1[i]&&b2[i]==a1[i]+b1[i]) ;
-        else if(a2[i]==a1[i]+b1[i]) b++;
-        else if(b2[i]==a1[i]+b1[i]) a++;
+        cin>>a1>>a2>>b1>>b2;
+        if(a2==a1+b1&&b2==a1+b1) ;
+        else if(a2==a1+b1) b++;
+        else if(b2==a1+b1) a++;
     }
     cout<<a<<' '<<b;
+    return 0;
 }
