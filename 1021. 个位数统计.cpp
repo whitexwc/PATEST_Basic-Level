3cfcaa2f79b6edb17fbe18fe9e622ae32d45cc3d
wucheng xiao <thinker55@163.com>
2017-02-25 15:41:33 +0800

    Update 1021. 个位数统计.cpp

--- 1021. 个位数统计.cpp
+++ 1021. 个位数统计.cpp
@@ -2,19 +2,17 @@
 using namespace std;
 int main()
 {
-    int a[]={0,1,2,3,4,5,6,7,8,9};
     int k[10]={0},i=0,j=0,m=0;
     string n;
     cin>>n;
     while(n[i])
     {
-        for(j=0;j<10;j++)
-            if(n[i]-'0'==a[j]) k[j]++;
+        k[(n[i]-'0')]++;
         ++i;
-    }
+    };
     for(j=0;j<10;j++)
     {
-        if(k[j]>0) cout<<a[j]<<':'<<k[j]<<endl;
+        if(k[j]>0) cout<<j<<':'<<k[j]<<endl;
     }
     return 0;
 }
