f2b8d2a1e4aeac3192b7d3f71ad64fb7274a84f3
wucheng xiao <thinker55@163.com>
2017-02-25 15:58:34 +0800

    Update 1031. 查验身份证.cpp

--- 1031. 查验身份证.cpp
+++ 1031. 查验身份证.cpp
@@ -5,7 +5,13 @@ bool check(string a)
     char m[]={'1','0','X','9','8','7','6','5','4','3','2'};
     int q[]={7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2};
     int i=0,sum=0,z;
-    for(;i<17;i++) sum+=(a[i]-'0')*q[i];
+    for(;i<17;i++) 
+    {
+        if(a[i]-'0'>9||a[i]-'0'<0) 
+            return false;
+        else 
+            sum+=(a[i]-'0')*q[i];
+    }
     z=sum%11;
     if(a[17]==m[z]) return true;
     else return false;
